92a8d2ce3f7e7b9f394069acfb466c6c8391638b
Lars Glud <larshg@gmail.com>
2020-11-07 18:41:50 +0100

    Fix visualization - Add compatability functions

--- visualization/include/pcl/visualization/qvtk_compatability.h
+++ visualization/include/pcl/visualization/qvtk_compatability.h
@@ -6,19 +6,45 @@
  *
  *  All rights reserved
  */
+#include <pcl/pcl_macros.h>
 #include <pcl/pcl_config.h>
 
 #if HAVE_QVTK
-  #include <vtkVersion.h>
+#include <vtkVersion.h>
 
-  #if VTK_MAJOR_VERSION > 8
-    #include <QVTKOpenGLNativeWidget.h>
-    using PCLQVTKWidget = QVTKOpenGLNativeWidget;
-  #else
-    #include <QVTKWidget.h>
-    using PCLQVTKWidget = QVTKWidget;
-  #endif
+#if VTK_MAJOR_VERSION > 8
+  #include <QVTKOpenGLNativeWidget.h>
+  using PCLQVTKWidget = QVTKOpenGLNativeWidget;
+#else
+  #include <QVTKWidget.h>
+  using PCLQVTKWidget = QVTKWidget;
+#endif
+
+
+inline auto PCL_EXPORTS getInteractorCompat(PCLQVTKWidget* qvtk) {
+#if VTK_MAJOR_VERSION > 8
+  return qvtk->interactor();
+#else
+  return qvtk->GetInteractor();
+#endif
+}
+
+inline auto PCL_EXPORTS getRenderWindowCompat(PCLQVTKWidget* qvtk) {
+#if VTK_MAJOR_VERSION > 8
+  return qvtk->renderWindow();
+#else
+  return qvtk->GetRenderWindow();
+#endif
+}
+
+inline auto PCL_EXPORTS setRenderWindowCompat(PCLQVTKWidget* qvtk, vtkRenderWindow* window) {
+#if VTK_MAJOR_VERSION > 8
+  return qvtk->setRenderWindow(window);
+#else
+  return qvtk->SetRenderWindow(window);
+#endif
+}
 
 #else
-  #error PCL is not compiled with QVTK.
+#error PCL is not compiled with QVTK.
 #endif
